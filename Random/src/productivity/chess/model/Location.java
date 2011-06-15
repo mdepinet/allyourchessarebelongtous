78cb48fec862948ae835d1b347d35ca55c4329c5
will.siegfried92@gmail.com <will.siegfried92@gmail.com@d0154375-89f0-89a8-2fc2-1211b56c8ac8>
2011-06-15 20:53:57 +0000

    Basic Board setup and SERIALIZED.

---
+++ Random/src/productivity/chess/model/Location.java
@@ -0,0 +1,23 @@
+package productivity.chess.model;
+
+public class Location {
+	private int col;
+	private int row;
+	public Location(int x, int y)
+	{
+		setCol(x);
+		setRow(y);
+	}
+	public void setCol(int x) {
+		this.col = x;
+	}
+	public int getCol() {
+		return col;
+	}
+	public void setRow(int y) {
+		this.row = y;
+	}
+	public int getRow() {
+		return row;
+	}
+}
