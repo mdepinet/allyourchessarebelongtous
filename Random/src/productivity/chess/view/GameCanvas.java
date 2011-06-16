31d18b69bac7a340fed86231b58c89af9faabbee
will.siegfried92@gmail.com <will.siegfried92@gmail.com@d0154375-89f0-89a8-2fc2-1211b56c8ac8>
2011-06-16 16:26:45 +0000

    Added highlighted move squares.

--- Random/src/productivity/chess/view/GameCanvas.java
+++ Random/src/productivity/chess/view/GameCanvas.java
@@ -2,6 +2,7 @@ package productivity.chess.view;
 
 import java.awt.*;
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
@@ -10,17 +11,19 @@ import productivity.chess.model.*;
 public class GameCanvas extends Canvas {
 	private static final long serialVersionUID = 1L;
 	private GameBoard board;
+	private ArrayList<Location> moves;
 	//private Image king, queen, bishop, knight, rook, pawn;
 	
 	public GameCanvas() {
 	  setBackground (Color.WHITE);
+	  moves = new ArrayList<Location>();
 	  board = new Board(); //Default board
 	  System.out.print(board);
 	}
 	 public void paint (Graphics g) {
 	   Graphics2D g2;
 	   g2 = (Graphics2D) g;
-	   
+	   g2.setColor(Color.BLACK);
 	   for(int i = 0; i < 8; i++)
 	   {
 		   for(int j = 0; j < 8; j++)
@@ -48,6 +51,11 @@ public class GameCanvas extends Canvas {
 			   }
 		   }
 	   }
+	   g2.setColor(new Color(1.0f,1.0f,0.0f,0.5f));
+	   for(Location loc: moves)
+	   {
+		   g2.fillRect(50+(loc.getRow()*30), 50+(loc.getCol()*30), 30, 30);
+	   }
 	 }
 
 	public void setBoard(GameBoard board) {
@@ -57,5 +65,15 @@ public class GameCanvas extends Canvas {
 
 	public GameBoard getBoard() {
 		return board;
+	}
+	public void clearMoves()
+	{
+		moves = new ArrayList<Location>();
+	}
+	public ArrayList<Location> getMoves() {
+		return moves;
+	}
+	public void setMoves(ArrayList<Location> moves) {
+		this.moves = moves;
 	}
 }
