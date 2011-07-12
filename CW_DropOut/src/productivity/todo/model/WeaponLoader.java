a8fbb2985cfde645c1568d150db6b5a0560cfde8
mikedabomb000 <mikedabomb000@d0154375-89f0-89a8-2fc2-1211b56c8ac8>
2011-07-12 20:09:41 +0000

    Added representative chars and default weapons

--- CW_DropOut/src/productivity/todo/model/WeaponLoader.java
+++ CW_DropOut/src/productivity/todo/model/WeaponLoader.java
@@ -9,24 +9,38 @@ import java.util.TreeMap;
 public class WeaponLoader {
 	private static String defaultWeaponSet = "resource/default.ws";
 	private static Map<String,WeaponDefinition> definitions;
+	private static Map<Character,WeaponDefinition> chars;
+	private static WeaponDefinition defaultWeapon;
 	
 	public static boolean load(Weapon wep, String name){
 		if (definitions == null || definitions.isEmpty()) loadAll();
+		if (name.equalsIgnoreCase("Default")){
+			wep.setProperties(defaultWeapon);
+			return true;
+		}
 		WeaponDefinition def = definitions.get(name);
 		wep.setProperties(def);
 		return def == null;
 	}
 	private static void loadAll(){
 		definitions = new TreeMap<String,WeaponDefinition>();
+		chars = new TreeMap<Character,WeaponDefinition>();
 		Scanner scan = null;
 		try{
 			scan = new Scanner(new File(defaultWeaponSet));
 			while (scan.hasNext()){
 				WeaponDefinition wepDef = new WeaponDefinition(scan.nextLine());
+				if (defaultWeapon == null) defaultWeapon = wepDef;
 				definitions.put(wepDef.getName(), wepDef);
+				chars.put(wepDef.getRepresentativeChar(), wepDef);
 			}
 		} catch (IOException ex){
 			ex.printStackTrace();
 		}
+	}
+	
+	public WeaponDefinition getWeaponDef(char c){
+		if (chars == null || chars.isEmpty()) loadAll();
+		return chars.get(c);
 	}
 }
