1f96cedb0017f99cbb1935545f5b04830d400d3d
will.siegfried92 <will.siegfried92@d0154375-89f0-89a8-2fc2-1211b56c8ac8>
2011-07-22 22:55:19 +0000

    Capture the Flag mode works

--- trunk/CW_DropOut/src/productivity/todo/config/CaptureTheFlagMode.java
+++ trunk/CW_DropOut/src/productivity/todo/config/CaptureTheFlagMode.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 import productivity.todo.model.GameMap;
+import productivity.todo.model.Player;
+import productivity.todo.model.Weapon;
 
 public class CaptureTheFlagMode extends GameMode {
 
@@ -42,8 +44,26 @@ public class CaptureTheFlagMode extends GameMode {
 
 	@Override
 	public int getWinningTeam() {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < gameMap.getPlayers().size();i++) {
+			Player p = gameMap.getPlayers().get(i);
+			if(p.getCurrentWeapon().getName().indexOf("Flag")!=-1)
+			{
+				if(p.getLocation().distance(getTeamFlagLocation(p.getTeam()))<GameMap.GRID_PIXELS-p.getRadius())
+					return p.getTeam();
+			}
+		}
 		return -1;
+	}
+	public Point2D.Double getTeamFlagLocation(int team)
+	{
+		for(int i = 0; i < gameMap.getMap().length;i++) {
+			for(int j = 0; j < gameMap.getMap()[i].length;j++) {
+				if(gameMap.getMap()[i][j] == 'X' || gameMap.getMap()[i][j] == '_') continue;
+				if(new Weapon(gameMap.getMap()[i][j]).getName().indexOf(""+team)!=-1)
+					return new Point2D.Double(i*25+GameMap.GRID_PIXELS/2, j*25 + GameMap.GRID_PIXELS/2);
+			}
+		}
+		return null;
 	}
 
 }
