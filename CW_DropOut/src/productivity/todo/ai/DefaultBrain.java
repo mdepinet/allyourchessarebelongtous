84c96994b030c30cbaf45b4cace1fed543733229
mikedabomb000 <mikedabomb000@d0154375-89f0-89a8-2fc2-1211b56c8ac8>
2011-07-26 21:06:39 +0000

    Grey matter

--- CW_DropOut/src/productivity/todo/ai/DefaultBrain.java
+++ CW_DropOut/src/productivity/todo/ai/DefaultBrain.java
@@ -16,8 +16,8 @@ public class DefaultBrain extends AbstractBrain {
 		Point2D.Double location = p.getLocation();
 		Point2D.Double newLoc;
 		Point2D.Double destLoc;
-		if(!objectives.isEmpty()) destLoc = map.fromGridPoint(objectives.get(0).getLocation());
-		else {
+//		if(!objectives.isEmpty()) destLoc = map.fromGridPoint(objectives.get(0).getLocation());
+//		else {
 			if(enemy!=null) {
 				Point2D.Double pLoc = enemy.getLocation();
 				Point2D.Double wLoc = getClosestWeaponLoc(map, p);
@@ -26,25 +26,23 @@ public class DefaultBrain extends AbstractBrain {
 			else {
 				destLoc = getClosestWeaponLoc(map, p);
 			}
-		}
+//		}
 		if(destLoc!=null)
 		{
-			newLoc = addPoints(location,getSmartDirectionToLoc(location,destLoc,map));
+			newLoc = getSmartDirectionToLoc(location,destLoc,map);
 			if(enemy!=null)
 			{
-				turn(getAngleBetweenPoints(location,enemy.getLocation())+Math.PI/2,p);
+				turn(getOrientationToPoint(location,enemy.getLocation()),p);
 				if(p.getNumWeapons()>1 && p.getCurrentWeapon().getType().equals("Pistol"))
 					p.nextWeapon();
 				if(enemy.getHealth()>0)
 				{
 					if(location.distance(enemy.getLocation())<=enemy.getCurrentWeapon().getEffRange())
 					{
-						if(Math.abs(Math.sin(p.getOrientation()+Math.PI) - Math.sin(enemy.getOrientation()))<=Math.PI/5)
-							newLoc = addPoints(location,multiplyPointByScalar(getSmartDirectionToLoc(location,enemy.getLocation(),map),1));
+							newLoc = scaleVector(getDirectionToLoc(location,enemy.getLocation()),-1);
 					}
 					if(location.distance(enemy.getLocation())<=p.getCurrentWeapon().getEffRange()*1.5)
 					{
-						if(p.getCurrentWeapon().canShoot())
 							shoot(map,p);
 					}
 				}
@@ -56,7 +54,6 @@ public class DefaultBrain extends AbstractBrain {
 		}
 	}
 	private double getCostForPath(Point from, Point to) {
-		
 		return from.distance(to);
 	}
 	private void addObjectives(GameMap map, Player p) {
