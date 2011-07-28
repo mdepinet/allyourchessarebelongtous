13e61eebc8207fb0eb293b0e313edf1fc52d8009
emh0729 <emh0729@d0154375-89f0-89a8-2fc2-1211b56c8ac8>
2011-07-28 21:56:09 +0000

    fixed zombies, made a zombie brain(which is just a duplicate of default brain, made weapons dropped disappear after 15

---
+++ trunk/CW_DropOut/goodSrc/org/cwi/shoot/ai/ZombieBrain.java
@@ -0,0 +1,65 @@
+package org.cwi.shoot.ai;
+
+import java.awt.geom.Point2D;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+import org.cwi.shoot.ai.objective.Objective;
+import org.cwi.shoot.config.GameMode;
+import org.cwi.shoot.map.GameMap;
+import org.cwi.shoot.model.Player;
+import org.cwi.shoot.model.Weapon;
+import org.cwi.shoot.util.VectorTools;
+
+public class ZombieBrain extends AbstractBrain {
+	private static final int OBJECTIVE_REFRESH = 50;
+	private static final int ACTIVE_WEIGHT = 50;
+	private int objectiveTurns = 50;
+	private Objective activeObjective = null;
+	
+	public void makeMove(GameMode mode, GameMap map, Player p){
+		if (objectiveTurns >= OBJECTIVE_REFRESH){
+			getObjectives(mode, map, p);
+			addDefaultObjectives(map, p);
+			Collections.sort(objectives, new ObjectiveComparator());
+			objectiveTurns = 0;
+		}
+		objectiveTurns++;
+		Player enemy = getClosestEnemy(map,p);
+		Point2D.Double location = p.getLocation();
+		Point2D.Double newLoc;
+		Point2D.Double destLoc = null;
+		
+		//Pick location.  Objective -> Enemy or Weapon -> Weapon
+		if(!objectives.isEmpty()) destLoc = objectives.get(0).getTargetPoint(p);
+		
+		if(destLoc!=null){
+			newLoc = getSmartDirectionToLoc(location,destLoc,map);
+			if(enemy!=null) {
+				turn(VectorTools.getOrientationToPoint(location,enemy.getLocation()),p);
+				if(p.getNumWeapons()>1 && p.getCurrWeapon().getType() == Weapon.WeaponType.PISTOL) p.nextWeapon();
+				if(enemy.getHealth()>0) {
+//					if(enemy.getCurrWeapon() != null && location.distance(enemy.getLocation())<=enemy.getCurrWeapon().getEffRange()) {
+//							newLoc = scaleVector(getDirectionToLoc(location,enemy.getLocation()),-1);
+//					}
+					if(p.getCurrWeapon() != null && location.distance(enemy.getLocation())<=p.getCurrWeapon().getEffRange()*1.5) {
+							shoot(map,p);
+					}
+				}
+			}
+			move(newLoc,p);
+		}
+	}
+	private void getObjectives(GameMode mode, GameMap map, Player p) {
+		objectives = mode.getObjectives(map, p);
+		if (objectives == null) objectives = new LinkedList<Objective>();
+	}
+	
+	class ObjectiveComparator implements Comparator<Objective>{
+		@Override
+		public int compare(Objective o1, Objective o2) {
+			return o1.compareTo(o2) + (o1 == activeObjective ? ACTIVE_WEIGHT : o2 == activeObjective ? -ACTIVE_WEIGHT : 0);
+		}
+	}
+}
--- trunk/CW_DropOut/goodSrc/org/cwi/shoot/config/ZombiesWGuns.java
+++ trunk/CW_DropOut/goodSrc/org/cwi/shoot/config/ZombiesWGuns.java
@@ -9,60 +9,63 @@ import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
+import org.cwi.shoot.ai.DefaultBrain;
 import org.cwi.shoot.ai.objective.Objective;
 import org.cwi.shoot.map.GameMap;
 import org.cwi.shoot.model.Player;
 import org.cwi.shoot.model.Player.PlayerType;
 import org.cwi.shoot.model.Weapon;
 import org.cwi.shoot.model.Weapon.WeaponType;
+import org.cwi.shoot.threads.RespawnThread;
 import org.cwi.shoot.util.NameGenerator;
 
 public class ZombiesWGuns extends GameMode {
 	public static final int NUM_ENEMIES = 10;
-	public static final int ADD_NUM_ZOMBIES = 10;
+	public static final int ADD_NUM_ZOMBIES = 20;
 	public static final int WAVE_TIME_LIMIT = 20;
 	public static final int ZOMBIES_TEAM_NUM = 0;
 	private long startTime;
 	private int wave;
 	private long waveStartTime;
 	private List<Player> deadZombies;
-	private boolean addPlayers;
+	private List<Point> spawnLocs;
 	
 	public ZombiesWGuns() {
 		startTime = System.currentTimeMillis();
 		waveStartTime = System.currentTimeMillis();
 		wave = 1;
 		deadZombies = new ArrayList<Player>();
-		addPlayers = false;
+		spawnLocs = new ArrayList<Point>();
 	}
 	
 	public void onStartup(GameMap map, GameOptions setup){
+		for(int r = 0; r < modeMap.length; r++) 
+			for(int c = 0; c < modeMap[r].length; c++)
+				if(modeMap[r][c]=='0')
+					spawnLocs.add(new Point(r,c));
 		map.spawn(map.getPlayer());
-		List<Player> z = addZombies(NUM_ENEMIES);
-		for(int i = 0; i < z.size(); i++) {
-			Player zombie = z.get(i);
-			if(map.getSpawnLocs().get(ZOMBIES_TEAM_NUM)!=null && !map.getSpawnLocs().get(zombie.getTeam()).isEmpty()) {
-				map.getPlayers().add(zombie);
-				map.spawn(zombie);
-			}
-		}
+		/*List<Player> z = addZombies(NUM_ENEMIES);
+		map.getPlayers().addAll(z);*/
 			
 	}
 	public void onReset(GameMap map, GameOptions setup){
 		map.getPlayers().clear();
+		for(int i = 0; i < map.getThreads().size(); i++) { 
+			RespawnThread t = map.getThreads().get(i); 
+			t.respawn(); 
+			t.kill(); 
+		}
+		map.getThreads().clear();
+		
 		Player player = new Player(setup.getPlayerName());
 		player.setTeam(setup.getPlayerTeam());
 		player.setType(Player.PlayerType.HUMAN);
-		map.getPlayers().add(player);
+		map.getPlayers().add(0,player);
 		map.spawn(player);
-		List<Player> z = addZombies(NUM_ENEMIES);
-		for(int i = 0; i < z.size(); i++) {
-			Player zombie = z.get(i);
-			if(map.getSpawnLocs().get(ZOMBIES_TEAM_NUM)!=null && !map.getSpawnLocs().get(zombie.getTeam()).isEmpty()) {
-				map.getPlayers().add(zombie);
-				map.spawn(zombie);
-			}
-		}
+		
+		/*List<Player> z = addZombies(NUM_ENEMIES);
+		map.getPlayers().addAll(z);*/
+		
 		setWave(1);
 		setWaveStartTime(System.currentTimeMillis());
 		setStartTime(System.currentTimeMillis());
@@ -93,26 +96,13 @@ public class ZombiesWGuns extends GameMode {
 			Player foe = new Player(gen.compose((int)(Math.random()*3)+2));
 			foe.setTeam(ZOMBIES_TEAM_NUM);
 			foe.setType(PlayerType.COMPUTER);
+			foe.setLocation(GameMap.fromGridPoint(spawnLocs.get((int)(spawnLocs.size()*Math.random()))));
+			foe.addWeapon(new Weapon("Default"), this);
+			foe.setStats(new DefaultBrain());
 			zombiesToAdd.add(foe);
-			/*gameMap.getPlayers().add(foe);
-			gameMap.spawn(foe);*/
+			
 		}
 		return zombiesToAdd;
-	}
-	public List<Player> getPlayersToAdd() {
-		if(addPlayers) {
-			addPlayers = false;
-			List<Player> addZombies = addZombies(ADD_NUM_ZOMBIES);
-			if(deadZombies.size()>0) {
-				for(int i = 0; i < deadZombies.size(); i++)
-					if(deadZombies.get(i).getHealth()<=0) {
-						addZombies.add(deadZombies.get(i));
-						deadZombies.remove(i--);
-					}
-			}
-			return addZombies;
-		}
-		return null;
 	}
 	public List<Player> getDeadZombies() {
 		return deadZombies;
@@ -146,7 +136,7 @@ public class ZombiesWGuns extends GameMode {
 
 	@Override
 	public String getScoreForPlayer(org.cwi.shoot.model.Player player) {
-		return player.getName() + ": " + player.getStats().getNumKills();
+		return player.getType()==PlayerType.COMPUTER ? "" : player.getName() + ": " + player.getStats().getNumKills();
 	}
 
 	@Override
@@ -158,11 +148,17 @@ public class ZombiesWGuns extends GameMode {
 	@Override
 	public void update(List<org.cwi.shoot.model.Player> players) {
 		if((System.currentTimeMillis() - getWaveStartTime())/1000. >= WAVE_TIME_LIMIT) {
-			//addZombies(ADD_NUM_ZOMBIES);
-			//respawnZombies();
+			List<Player> addZombies = addZombies(ADD_NUM_ZOMBIES);
+			if(deadZombies.size()>0) {
+				for(int i = 0; i < deadZombies.size(); i++)
+					if(deadZombies.get(i).getHealth()<=0) {
+						addZombies.add(deadZombies.get(i));
+						deadZombies.remove(i--);
+					}
+			}
+			players.addAll(addZombies);
 			setWave(getWave()+1);
 			setWaveStartTime(System.currentTimeMillis());
-			addPlayers = true;
 		}
 	}
 
@@ -184,7 +180,9 @@ public class ZombiesWGuns extends GameMode {
 
 	@Override
 	public List<Character> getAdditionalMapChars() {
-		return new ArrayList<Character>();
+		List<Character> specChars = new ArrayList<Character>();
+		specChars.add('0');
+		return specChars;
 	}
 
 	@Override
@@ -195,6 +193,7 @@ public class ZombiesWGuns extends GameMode {
 	@Override
 	public List<Objective> getObjectives(org.cwi.shoot.map.GameMap map,
 			org.cwi.shoot.model.Player p) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -207,20 +206,17 @@ public class ZombiesWGuns extends GameMode {
 
 	@Override
 	public void onPlayerRespawn(org.cwi.shoot.model.Player p) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void drawModeMapPre(Graphics2D g) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void drawModeMapPost(Graphics2D g,
 			List<org.cwi.shoot.model.Player> players) {
-		// TODO Auto-generated method stub
 		
 	}
 	
