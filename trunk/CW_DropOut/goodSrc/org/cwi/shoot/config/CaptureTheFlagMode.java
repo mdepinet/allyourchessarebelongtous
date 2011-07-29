a118dbf28dcd11d8123eb7464cb9927fdb7e76eb
will.siegfried92 <will.siegfried92@d0154375-89f0-89a8-2fc2-1211b56c8ac8>
2011-07-29 23:00:30 +0000

    made CaptureTheFlagMode the best mode by adding objectives

--- trunk/CW_DropOut/goodSrc/org/cwi/shoot/config/CaptureTheFlagMode.java
+++ trunk/CW_DropOut/goodSrc/org/cwi/shoot/config/CaptureTheFlagMode.java
@@ -10,6 +10,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.cwi.shoot.ai.objective.KillObjective;
+import org.cwi.shoot.ai.objective.LocationObjective;
 import org.cwi.shoot.ai.objective.Objective;
 import org.cwi.shoot.map.GameMap;
 import org.cwi.shoot.model.Player;
@@ -18,8 +20,15 @@ import org.cwi.shoot.model.Weapon.WeaponType;
 
 public class CaptureTheFlagMode extends GameMode {
 	private static final int capturesToWin = 3;
+	private static final double OBJECTIVE_WEIGHT = 6;
+	private static final double DEFEND_OBJECTIVE_WEIGHT = 8;
 	private Map<Player, Integer> flagsCaptured;
+	private Map<Integer, Point> currentFlagLocs;
 	private Map<Integer, Point> flagSpawnLocs;
+	enum CaptureState {
+		CAPTURE, RETURN
+	}
+	private CaptureState[] teamStates;
 	//indices of the longs are the teams which those flags belong to
 	private long[] flagRespawnTimes;
 	private static final Character[] FLAG_CHARS= {'L','M','N','O'};
@@ -27,7 +36,13 @@ public class CaptureTheFlagMode extends GameMode {
 	public CaptureTheFlagMode(){
 		flagsCaptured = new HashMap<Player,Integer>();
 		flagSpawnLocs = new HashMap<Integer, Point>();
+		currentFlagLocs = new HashMap<Integer, Point>();
 		flagRespawnTimes= new long[4];
+		teamStates = new CaptureState[4];
+		for(int i = 0 ; i < 4; i++) teamStates[i] = CaptureState.CAPTURE;
+	}
+	private void resetTeamStates() {
+		for(int i = 0 ; i < 4; i++) teamStates[i] = CaptureState.CAPTURE;
 	}
 	@Override
 	public String getModeName() {
@@ -44,6 +59,7 @@ public class CaptureTheFlagMode extends GameMode {
 					if (myChars.contains(charMap[r][c])){
 						modeMap[r][c] = charMap[r][c];
 						flagSpawnLocs.put(modeMap[r][c]-75, new Point(r,c));
+						currentFlagLocs.put(modeMap[r][c]-75, new Point(r,c));
 						charMap[r][c] = '_';
 					}
 				}
@@ -56,6 +72,12 @@ public class CaptureTheFlagMode extends GameMode {
 		}
 	}
 
+	@Override
+	public void onReset(GameMap map, GameOptions setup){
+		super.onReset(map, setup);
+		resetMode(map.getPlayers());
+	}
+	
 	@Override
 	public String getScoreForPlayer(Player player) {
 		return player.getName()+": "+ (flagsCaptured.get(player)==null ? 0 : flagsCaptured.get(player));
@@ -89,6 +111,7 @@ public class CaptureTheFlagMode extends GameMode {
 					Weapon w = new Weapon(c,gridPoint);
 					if(canGetWeapon(p,w)) {
 						p.addWeapon(w, this);
+						teamStates[p.getTeam()-1] = CaptureState.RETURN;
 						flagRespawnTimes[w.getCharacter()-76] = 0;
 						p.switchToWeapon(p.getNumWeapons()-1);
 						modeMap[gridPoint.x][gridPoint.y] = GameOptions.BLANK_CHARACTER;
@@ -105,6 +128,7 @@ public class CaptureTheFlagMode extends GameMode {
 							if(modeMap[r][c]==(char)(i+76))
 								modeMap[r][c]=GameOptions.BLANK_CHARACTER;
 					modeMap[flagSpawnLocs.get(i+1).x][flagSpawnLocs.get(i+1).y] = (char)(i+76);
+					currentFlagLocs.put(i+1, flagSpawnLocs.get(i+1));
 				}
 					
 		}
@@ -118,8 +142,11 @@ public class CaptureTheFlagMode extends GameMode {
 		for(int i = 0; i < modeMap.length;i++)
 			for(int j = 0; j < modeMap[i].length;j++)
 				modeMap[i][j] = GameOptions.BLANK_CHARACTER;
-		for(int i:flagSpawnLocs.keySet())
+		for(int i:flagSpawnLocs.keySet()) {
 			modeMap[flagSpawnLocs.get(i).x][flagSpawnLocs.get(i).y] = (char)(i+75);
+			currentFlagLocs.put(i, flagSpawnLocs.get(i));
+		}
+		resetTeamStates();
 	}
 	private boolean isFlag(Weapon w){
 		return w.getTypes().contains(WeaponType.OBJECTIVE);
@@ -165,19 +192,39 @@ public class CaptureTheFlagMode extends GameMode {
 
 	@Override
 	public List<Objective> getObjectives(GameMap map, Player p) {
-		// TODO: Needs optimization- there is no reason to go through the entire 30x30 grid to find 4 flags
-		//for(int r = 0; r<modeMap.length;r++)
-		//	for(int c =0; c<modeMap[r].length;c++){
-		//		if(modeMap[r][c]!=0)
-		//			p.addObjective(new Objective(new Point(r,c), 1, 1));
-		//	}
-		return new LinkedList<Objective>();
+		List<Objective> ret = new LinkedList<Objective>();
+		switch(teamStates[p.getTeam()-1]) {
+			case CAPTURE: 
+				for(int i : currentFlagLocs.keySet())
+					if(i!=p.getTeam())
+						ret.add(new LocationObjective(OBJECTIVE_WEIGHT, GameMap.getGridPoint(p.getLocation()).distance(currentFlagLocs.get(i)), GameMap.fromGridPoint(currentFlagLocs.get(i))));
+				break;
+			case RETURN:
+				if(p.hasFlag())
+					ret.add(new LocationObjective(OBJECTIVE_WEIGHT*2, GameMap.getGridPoint(p.getLocation()).distance(flagSpawnLocs.get(p.getTeam())), GameMap.fromGridPoint(flagSpawnLocs.get(p.getTeam()))));
+				else
+					for(int i = 0; i < map.getPlayers().size();i++) {
+						Player player = map.getPlayers().get(i);
+						if(player.getTeam()==p.getTeam() && player.hasFlag())
+							ret.add(new LocationObjective(OBJECTIVE_WEIGHT, GameMap.getGridPoint(p.getLocation()).distance(GameMap.getGridPoint(player.getLocation())), player.getLocation()));
+					}
+					break;
+			default: break;	
+		}
+		for(int i = 0; i < map.getPlayers().size();i++) {
+			Player player = map.getPlayers().get(i);
+			if(player.hasTeamFlag(p.getTeam()))
+				ret.add(new KillObjective(DEFEND_OBJECTIVE_WEIGHT, GameMap.getGridPoint(p.getLocation()).distance(GameMap.getGridPoint(player.getLocation())), player));
+		}
+		return ret;
 	}
 
 	@Override
 	public void onPlayerDeath(Player p) {
 		if(playerHasFlag(p)){
 			Point loc = GameMap.getGridPoint(p.getLocation());
+			currentFlagLocs.put(p.getCurrWeapon().getCharacter()-75, new Point(loc.x, loc.y));
+			teamStates[p.getTeam()-1] = CaptureState.CAPTURE;
 			modeMap[loc.x][loc.y]=p.getCurrWeapon().getCharacter();
 			flagRespawnTimes[p.getCurrWeapon().getCharacter()-76] = System.currentTimeMillis();
 		}
