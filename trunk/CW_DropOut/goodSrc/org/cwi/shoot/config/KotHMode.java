856c31802e65d9a59e94987946a07c9515a9ad0f
emh0729 <emh0729@d0154375-89f0-89a8-2fc2-1211b56c8ac8>
2011-08-03 19:02:49 +0000

    King of the Hill

--- trunk/CW_DropOut/goodSrc/org/cwi/shoot/config/KotHMode.java
+++ trunk/CW_DropOut/goodSrc/org/cwi/shoot/config/KotHMode.java
@@ -6,9 +6,12 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.cwi.shoot.ai.objective.KillObjective;
+import org.cwi.shoot.ai.objective.LocationObjective;
 import org.cwi.shoot.ai.objective.Objective;
 import org.cwi.shoot.map.GameMap;
 import org.cwi.shoot.model.Player;
@@ -16,22 +19,50 @@ import org.cwi.shoot.model.Weapon;
 import org.cwi.shoot.threads.RespawnThread;
 
 public class KotHMode extends GameMode {
-	public static final long TIME_TO_WIN = 30;
+	public static final long TIME_TO_WIN = 91;
 	public static final Character[] ZONE_CHARS = { 'X' };
+	private static final double OBJECTIVE_WEIGHT = 15;
+	private static final double DEFEND_OBJECTIVE_WEIGHT = 20;
+	public static final long HILL_IDLE_TIME = 5;
+
+	private Map<Integer, Integer> teamPoints = new HashMap<Integer, Integer>();
 	private Map<Point, Character> zone;
 	private int teamControl;
-	private long teamTime;
+	private long startTeamTime;
+	private int teamTime;
+	private List<Player> inTheZone;
+	private long timeIdle;
 	
 	public KotHMode() {
 		zone = new HashMap<Point, Character>();
 		teamControl = -1;
-		teamTime = System.currentTimeMillis();
+		teamTime = -1;
+		startTeamTime = -1;
+		inTheZone = new ArrayList<Player>();
+		timeIdle = 0;
+	}
+	
+	public void moveHill() {
+		zone = new HashMap<Point, Character>();
+		int i = (int)(Math.random() * 4)+2;
+		Collection<Character> myChars = getAdditionalMapChars();
+		int spotRow = (int)(modeMap.length * Math.random());
+		int spotCol = (int)(modeMap[spotRow].length * Math.random());
+		for (int r = spotRow; r < (spotRow + i > modeMap.length ? modeMap.length : spotRow + i); r++){
+			for (int c = spotCol; c < (spotCol + i > modeMap[r].length ? modeMap[r].length : spotCol + i); c++){
+				if (!myChars.contains(modeMap[r][c])){
+					//modeMap[r][c] = modeMap[r][c];
+					zone.put(new Point(r,c), modeMap[r][c]);
+					//charMap[r][c] = '_';
+				}
+			}
+		}
 	}
 	
 	@Override
 	public void loadGameObjects(GameMap map) {
 		if(zone.keySet().isEmpty()) {
-			int i = (int)(Math.random() * (int)(1/Math.random())*3)+1;
+			int i = (int)(Math.random() * 4)+2;
 			char[][] charMap = map.getMap();
 			Collection<Character> myChars = getAdditionalMapChars();
 			modeMap = new char[charMap.length][charMap[0].length];
@@ -44,10 +75,6 @@ public class KotHMode extends GameMode {
 						zone.put(new Point(r,c), modeMap[r][c]);
 						charMap[r][c] = '_';
 					}
-//					else {
-//						relocateZone(map);
-//						return;
-//					}
 				}
 			}
 		}
@@ -89,6 +116,10 @@ public class KotHMode extends GameMode {
 		}
 		zone.clear();
 		loadGameObjects(map);
+		teamPoints = new HashMap<Integer, Integer>();
+		teamTime = -1;
+		startTeamTime = -1;
+		teamControl = -1;
 	}
 
 	@Override
@@ -103,9 +134,9 @@ public class KotHMode extends GameMode {
 
 	@Override
 	public String getScoreForTeam(int team, List<Player> players) {
-		// TODO Auto-generated method stub
-		if(team == teamControl) return "" + (System.currentTimeMillis()-teamTime)/1000;
-		return null;
+		Integer i = teamPoints.get(team);
+		i = (i == null ? 0 : i);
+		return ""+(teamControl != -1 && teamControl == team ? i+teamTime : i);
 	}
 
 	@Override
@@ -114,28 +145,59 @@ public class KotHMode extends GameMode {
 			for(Player p : players) {
 				if(zone.containsKey(GameMap.getGridPoint(p.getLocation()))) {
 					teamControl = p.getTeam();
-					teamTime = System.currentTimeMillis();
+					startTeamTime = startTeamTime==-1 ? System.currentTimeMillis() : startTeamTime;
+					teamTime = (int) ((System.currentTimeMillis() - startTeamTime)/1000);
+					inTheZone.add(p);
+					timeIdle = -1;
 				}
+				else if(inTheZone.contains(p) && !zone.containsKey(GameMap.getGridPoint(p.getLocation())))
+					inTheZone.remove(p);
+			}
+			timeIdle = timeIdle == -1 ? System.currentTimeMillis() : timeIdle;
+			if(((System.currentTimeMillis() - timeIdle)/1000) > HILL_IDLE_TIME) {
+				timeIdle = -1;
+				moveHill();
 			}
 		}
 		else {
 			boolean teamInZone = false;
+			boolean contested = false;
 			for(Player p : players) {
 				if(p.getTeam()==teamControl && zone.containsKey(GameMap.getGridPoint(p.getLocation()))) {
 					teamInZone = true;
+					inTheZone.add(p);
 				}
+				else if(p.getTeam()!=teamControl && zone.containsKey(GameMap.getGridPoint(p.getLocation()))) contested = true;
 			}
-			if(!teamInZone) {
+			if(teamInZone && !contested) teamTime = (int) ((System.currentTimeMillis() - startTeamTime)/1000);
+			else if(teamInZone && contested) {
+				Integer i = teamPoints.get(teamControl);
+				i = (i == null ? 0 : i);
+				teamPoints.put(teamControl, (int) (teamControl != -1 ? i+teamTime : i));
+				teamControl = -1;
+				teamTime = -1;
+				startTeamTime = -1;
+			}
+			else if(!teamInZone) {
 				boolean inZone = false;
 				for(Player p : players) {
 					if(zone.containsKey(GameMap.getGridPoint(p.getLocation()))) {
+						Integer i = teamPoints.get(teamControl);
+						i = (i == null ? 0 : i);
+						teamPoints.put(teamControl, (int) (teamControl != -1 ? i+teamTime : i));
 						teamControl = p.getTeam();
-						teamTime = System.currentTimeMillis();
+						startTeamTime = System.currentTimeMillis();
+						teamTime = (int) ((System.currentTimeMillis() - startTeamTime)/1000);
 					}
 				}
 				if(!inZone) {
+					Integer i = teamPoints.get(teamControl);
+					i = (i == null ? 0 : i);
+					teamPoints.put(teamControl, (int) (teamControl != -1 ? i+teamTime : i));
 					teamControl = -1;
-					teamTime = 0;
+					teamTime = -1;
+					startTeamTime = -1;
+					inTheZone.clear();
 				}
 			}
 		}
@@ -143,13 +205,15 @@ public class KotHMode extends GameMode {
 
 	@Override
 	public int getWinningTeam(List<Player> players) {
-		if(teamControl!=-1 && (System.currentTimeMillis()-teamTime)/1000 >= TIME_TO_WIN) return teamControl;
+		if (teamTime >= TIME_TO_WIN) return teamControl;
+		for (Map.Entry<Integer,Integer> entry : teamPoints.entrySet()){
+			if (entry.getValue() + ((teamControl == entry.getKey()) ? teamTime : 0) >= TIME_TO_WIN) return entry.getKey();
+		}
 		return -1;
 	}
 
 	@Override
 	public boolean canGetWeapon(Player p, Weapon w) {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
@@ -164,14 +228,43 @@ public class KotHMode extends GameMode {
 
 	@Override
 	public int getMaxNumTeams() {
-		// TODO Auto-generated method stub
 		return 8;
 	}
 
 	@Override
 	public List<Objective> getObjectives(GameMap map, Player p) {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedList<Objective> objectives = new LinkedList<Objective>();
+		if (teamControl == -1){
+			List<Point> locs = new ArrayList<Point>();
+			for(Point pp : zone.keySet())
+				locs.add(pp);
+			Point point = locs.get((int)(Math.random()*locs.size()));
+			if (point != null) objectives.add(new LocationObjective(OBJECTIVE_WEIGHT,GameMap.getGridPoint(p.getLocation()).distance(point),GameMap.fromGridPoint(point)));
+		}
+		else{
+			List<Point> locs = new ArrayList<Point>();
+			for(Point pp : zone.keySet())
+				locs.add(pp);
+			Point point = locs.get((int)(Math.random()*locs.size()));
+			if (p.getTeam() != teamControl){
+				//Run away somehow
+				for(int i = 0; i < inTheZone.size(); i++)
+					if(inTheZone.get(i).getTeam()!=p.getTeam())
+						objectives.add(new KillObjective(OBJECTIVE_WEIGHT,GameMap.getGridPoint(p.getLocation()).distance(point),inTheZone.get(i)));
+				Player target = map.getClosestNonTeamPlayer(p.getTeam(), p.getLocation());
+				if(target!=null)
+					objectives.add(new KillObjective(OBJECTIVE_WEIGHT,GameMap.getGridPoint(p.getLocation()).distance(point),target));
+			}
+			else if(inTheZone.indexOf(p)==0) {
+				objectives.add(new LocationObjective(DEFEND_OBJECTIVE_WEIGHT,GameMap.getGridPoint(p.getLocation()).distance(point),GameMap.fromGridPoint(point)));
+			}
+			else if(!zone.containsKey(GameMap.getGridPoint(p.getLocation())) && !p.equals(inTheZone.get(0))){
+				Player target = map.getClosestNonTeamPlayer(p.getTeam(), inTheZone.get(0).getLocation());
+				if(target!=null)
+					objectives.add(new KillObjective(DEFEND_OBJECTIVE_WEIGHT,GameMap.getGridPoint(p.getLocation()).distance(point),target));
+			}
+		}
+		return objectives;
 	}
 
 	@Override
